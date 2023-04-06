80fbf7bcd59443ad5dd0264574724a30f79502a3
Vesa Zhitia <118364175+vesax@users.noreply.github.com>
2023-04-06 12:50:07 +0200

    Add files via upload
    loops

--- beaufort.h
+++ beaufort.h
@@ -10,6 +10,18 @@ string beaufort(string text, string key) {
     string cipher;
     string alfabeti = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
 
+    for (int i = 0; i < text.size(); i++) {
+        
+        for (int j = 0; j < 26; j++) {
+            
+            }
+        }
+        
+        for (int j = fillo; j < fillo + 26; j++) {
+        
+        
+    }
+   
  
 }
 
