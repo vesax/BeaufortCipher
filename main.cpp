2f40984b0eb4d8d8a02d86be5e2b7e4a08e8a0f1
Veronakabashi1 <116003282+Veronakabashi1@users.noreply.github.com>
2023-04-06 12:25:35 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -19,6 +19,22 @@ fillimi:
 
     switch (komanda) {
     case 1:
+        cout << "\tJepni plaintext-in : ";
+        cin >> text;
+
+        while (!all_of(text.begin(), text.end(), [](unsigned char c) { return isalpha(c); })) {
+            cout << "\tPlaintext-i duhet te permbaje vetem shkronja. Shkruani perseri: ";
+            cin >> text;
+        }
+        cout << "\tJepni celesin per enkriptim : ";
+        cin >> key;
+
+        while (!all_of(key.begin(), key.end(), [](unsigned char c) { return isalpha(c); })) {
+            cout << "\tCelesi duhet te permbaje vetem shkronja. Shkruani perseri: ";
+            cin >> key;
+        }
+        out = beaufort(text, key);
+        cout << "\n\tCiphertext-i : " << out << "\n";
         break;
     case 2:
         break;
