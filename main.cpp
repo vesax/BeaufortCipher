157a4297a6d1396b74e61c2b95d233037c745ed9
Veronakabashi1 <116003282+Veronakabashi1@users.noreply.github.com>
2023-04-06 12:23:14 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,7 +9,20 @@ int main() {
 
     cout << "\n\nProgrami per enkriptimin dhe dekriptimin e tekstit permes Beaufort Cipher!" << endl;
     tabela();
+fillimi:
+    cout << "\n\t\tMeny: " << endl;
+    cout << "\tShtypni 1 per enkriptim " << endl;
+    cout << "\tShtypni 2 per dekriptim " << endl;
+    cout << "\t";
+    cin >> komanda;
 
+
+    switch (komanda) {
+    case 1:
+        break;
+    case 2:
+        break;
+    }
 
 
     return 0;
