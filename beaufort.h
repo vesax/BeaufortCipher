8e5c3739db066c97e7a843ddebc8b345c5d27c4b
Agnesa-Hajdaraj <117785560+Agnesa-Hajdaraj@users.noreply.github.com>
2023-04-06 12:40:49 +0200

    Add files via upload

---
+++ beaufort.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <cctype> 
+#include <algorithm>
+using namespace std;
+
+string beaufort(string text, string key) {
+    string cipher;
+    string alfabeti = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+
+ 
+}
+
+
+
