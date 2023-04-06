f4ac744df07fef6e4cd8b655de3b46b33d232924
Vera Llugiqi <115923848+VeraLlugiqi@users.noreply.github.com>
2023-04-06 12:17:38 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include "beaufort.h"
+using namespace std;
+
+
+int main() {
+    string text, key, out;
+    int komanda;
+
+    cout << "\n\nProgrami per enkriptimin dhe dekriptimin e tekstit permes Beaufort Cipher!" << endl;
+    tabela();
+
+
+
+    return 0;
+}
